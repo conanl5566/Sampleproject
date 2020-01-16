71ac57f8a952bcd66c058aa51b7bb4ef2e7b11d5
conanl <conanl@CONANL-PC>
2020-01-16 11:08:39 +0800

    test1

--- src/dotNET.Web/Controllers/AccountController.cs
+++ src/dotNET.Web/Controllers/AccountController.cs
@@ -26,7 +26,7 @@ namespace CompanyName.ProjectName.Web.Host.Controllers
     ///
     /// </summary>
     [IgnoreAuthorize]
-    public class AccountController : Controller11
+    public class AccountController : Controller1122
     {
         #region ini
 
